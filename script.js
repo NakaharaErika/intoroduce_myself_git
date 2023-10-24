726c85568235e21e98800358c8f580070732fc67
NakaharaErika <148533889+NakaharaErika@users.noreply.github.com>
2023-10-24 15:53:30 +0900

    modal_OK

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-// アイコンのリンクがクリックされたときのイベントリスナー
-document.getElementById('imageLink').addEventListener('click', function(event) {
-    // デフォルトのリンクの動作を止める（新しいタブでの画像表示を防ぐ）
+
+//アイコンをクリックすると、こたろうくん出現
+//アイコンクリック
+let popupStart = document.getElementById("imageLink");
+popupStart.addEventListener("click",function(event){
+    event.preventDefault();//aタグはデフォルトでページ遷移するので阻止
+
+    const aHref = this.getAttribute("href");
+    const imgSrc = document.getElementById("popupImage");
+    imgSrc.setAttribute("src", aHref);//URLを挿入
+    document.getElementById("popup").style.display = "block"//styleをnoneからblockに
+})
+
+//ばつボタンクリック
+let popupEnd1 = document.getElementById("closeButton");
+popupEnd1.addEventListener("click",function(event){
     event.preventDefault();
 
-    // クリックされたアイコンのリンクから画像のURLを取得
-    const href = this.getAttribute('href');
-    // ポップアップ用のimgタグに取得した画像のURLをセット
-    document.getElementById('popupImage').setAttribute('src', href);
-    // ポップアップを表示
-    document.getElementById('popup').style.display = 'block';
-});
+    document.getElementById("popup").style.display = "none"
+})
 
-// 「×」ボタンがクリックされたときのイベントリスナー
-document.getElementById('closeButton').addEventListener('click', function(event) {
-    // デフォルトのリンクの動作を止める（画面移動を防ぐ）
-    event.preventDefault();
-
-    // ポップアップを非表示にする
-    document.getElementById('popup').style.display = 'none';
-});
-
-// ポップアップの背景（オーバーレイ）がクリックされたときのイベントリスナー
-document.getElementById('overlay').addEventListener('click', function() {
-    // ポップアップを非表示にする
-    document.getElementById('popup').style.display = 'none';
-});
+//モーダルクリックで元に戻る
+let popupEnd2 = document.getElementById("overlay");
+popupEnd2.addEventListener("click",function(){
+    document.getElementById("popup").style.display = "none"
+})
